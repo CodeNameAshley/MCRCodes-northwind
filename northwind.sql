25a43a96a6107c97f7b2c4b3653c70e979eaa36d
ashleyn <ashleynavo@googlemail.com>
2021-11-26 16:32:13 +0000

    sql commands and queries

--- northwind.sql
+++ northwind.sql
@@ -1,3 +1,27 @@
 USE northwind;
 
 SELECT * FROM employees;
+
+-- creating a simple table called countries
+CREATE TABLE countries (
+country_id INT,
+country_name VARCHAR(100),
+region_id INT
+);
+
+-- inserting a record into country table
+INSERT INTO countries (country_id, country_name, region_id) VALUES
+(001, 'Australia', 1);
+
+-- confirming record exists
+SELECT * FROM countries; 
+
+-- dropping table of countries
+DROP TABLE countries;
+
+-- crea
+CREATE TABLE countries (
+country_id INT NOT NULL,
+country_name VARCHAR(100) NOT NULL,
+region_id INT NOT NULL
+);
